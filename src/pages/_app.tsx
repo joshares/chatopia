b0402a3bbdd791682f2077eb9d478593f21e0c88
joshua <joshares9991@gmail.com>
2024-01-05 13:42:04 +0000

    react -query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import React from "react";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
+
+// hydrate(queryClient, dehydratedState);
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
